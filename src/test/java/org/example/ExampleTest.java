40de21e2d0b67ffd2df79b5dbcaf0554621b7767
gvlad <gvladovsky@gmail.com>
2022-09-13 23:08:56 +0300

    second test

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -1,2 +1,17 @@
-package org.example;public class ExampleTest {
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue (75 > 71);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue (77 > 76);
+    }
 }
